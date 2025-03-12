7331be97b409c2c9fff8b4f05047136059241ab1
Aaban Khan <aabankhan.404@gmail.com>
2025-03-12 22:43:51 +0530

    3rd day commit

---
+++ Day3.js
@@ -0,0 +1,12 @@
+let str1 = "aaban"
+let str2 = "aafan"
+
+// console.log(str1.indexOf("b"))
+// console.log(str2.charAt(4))
+// console.log(str1.slice(2,4))
+// console.log(str1.length)
+// console.log(str1.padStart(3,4));
+
+let din = Date()
+console.log(din.toLocalDateString())
+
